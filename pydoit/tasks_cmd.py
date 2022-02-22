51c7cf2d7a3fe24083c74ae7415d0ce45e9e9aab
sayantikabanik <sayantikabanik122@gmail.com>
2022-02-22 15:55:55 +0530

    nested version

--- pydoit/tasks_cmd.py
+++ pydoit/tasks_cmd.py
@@ -14,3 +14,20 @@ def task_modify():
     return {'actions': ["echo testing_write > output/foo.txt"],
             'file_dep': ["output/foo.txt"]
             }
+
+
+"""
+nested version
+"""
+
+
+def task_test():
+    name_file = input("Enter filename")
+    task_list = ["touch output/{}".format(name_file), "echo testing_write_123 > output/{}".format(name_file)]
+    for task_ in task_list:
+        task_name = task_.split(' ')[1]
+        yield{'name': task_name,
+              'doc': 'initial test task',
+              'actions': [task_],
+              'file_dep': ["output/foo.txt"]
+              }
